4c57026adaa8040d6bc01d5db21d820063706987
Emilio Martel <emilio_martel@outlook.es>
2024-12-17 18:17:47 +0000

    configure dotenv

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,5 @@
-import { Server } from "./presentation/server";
-
-
+import { envs } from "./config/plugins/envs.plugin";
+import { Server } from "./presentation/server"; 
 
 (() => {
 
@@ -10,6 +9,6 @@ import { Server } from "./presentation/server";
 
 function main() {
 
-  Server.start();
-  
+  //  Server.start();
+  // console.log( {envs} )
 }
