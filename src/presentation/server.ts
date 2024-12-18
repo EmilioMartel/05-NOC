644a804f8f58363c55bdc6cc26d2d0797fbb873c
Emilio Martel <emilio_martel@outlook.es>
2024-12-18 18:09:55 +0000

    servicio de envio de correo

--- src/presentation/server.ts
+++ src/presentation/server.ts
@@ -1,7 +1,9 @@
+import { envs } from "../config/plugins/envs.plugin";
 import { CheckService } from "../domain/use-cases/checks/check-service";
 import { FileSystemDataSource } from "../infrastructure/datasources/file-system.datasource";
 import { LogRepositoryImpl } from "../infrastructure/repositories/log.repository.impl";
 import { CronService } from "./cron/cron-service";
+import { EmailService } from "./email/email.service";
 
 
 const fileSystemLogRepository = new LogRepositoryImpl(
@@ -15,9 +17,22 @@ export class Server {
   public static start() {
 
     console.log('Server started...');
+    //todo: mandar email
+    // const emailService = new EmailService();
+    // emailService.sendEmailWithFileSystemLogs(
+    //   ['emiliomtg@gmail.com']
+    // );
     
-    //mandar email
+    // emailService.sendEmail({
+    //   to: 'emiliomtg@gmail.com',
+    //   subject: 'prueba de node - logs del sistema',
+    //   htmlBody: `
+    //   <h3>Logs del sistema - NOC </h3>
 
+    //   <p>Aliqua anim Lorem commodo irure do sint proident ut excepteur deserunt.</p>
+    //   <p>Ver logs adjuntos </p>
+    //   `
+    // });
 
     // CronService.createJob(
     //   '*/5 * * * * *',
